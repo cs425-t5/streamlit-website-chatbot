025a86700b027e712452a5373d4254ad84cc68b1
Clara tan yi shan <clara.tan.2022@scis.smu.edu.sg>
2024-11-01 16:54:24 +0800

    commit

---
+++ qa-sports-chatbot/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import utils
+
+# Streamlit UI
+st.subheader("🔎 Sports QA bot", divider="rainbow")
+st.caption(
+    """This website hosts a chatbot built using a Transformers architecture. It has been trained to answer questions related to rugby, soccer and basketball matches and players."""
+)
+
+# Who is the best basketball player?
+if user_query := st.text_input(
+    label="Ask a question about Rugby, Soccer or Basketball! 🏀",
+    placeholder="How many field goals did Kobe Bryant score?",
+):
+    # Get the answers
+    with st.spinner("Waiting"):
+        answer = utils.generate_ans(user_query)
+        st.divider()
---
+++ qa-sports-chatbot/utils.py
@@ -0,0 +1,65 @@
+"""This module contains utility functions for the project"""
+import streamlit as st
+import torch
+import torch.nn as nn
+from model import Seq2SeqTransformer
+
+from tokenizers import Tokenizer
+from tokenizers import decoders
+from pathlib import Path
+
+# Assuming the same base path where you saved it
+base = Path('tokenizer')
+tokenizer_path = str(base / 'sportsQA_context.json')
+
+# Load the tokenizer
+bert_tokenizer = Tokenizer.from_file('tokenizer/sportsQA_context.json')
+
+# Specify CPU device
+device = torch.device('cpu')
+
+# Load the saved model configuration
+config = {
+    'dim': 768,
+    'n_heads': 12,
+    'attn_dropout': 0.1,
+    'mlp_dropout': 0.1,
+    'depth': 6,
+    'vocab_size': 30000,
+    'max_len': 128,
+    'pad_token_id': 0  # Set to 0 explicitly
+}
+
+# Initialize model
+model = Seq2SeqTransformer(config)
+
+# Load the state dictionary
+state_dict = torch.load('best_model.pt', map_location=device)
+model.load_state_dict(state_dict)
+model.eval()
+
+def generate_ans(text_input):
+    if text_input is not None:
+        try:
+            # Debug prints
+            print(bert_tokenizer.encode(f"<bos>{text_input}<eos>"))
+            input_ids = bert_tokenizer.encode(f"<bos>{text_input}<eos>").ids
+            input_ids = torch.tensor(input_ids,dtype=torch.long).unsqueeze(0)
+
+            bos = bert_tokenizer.token_to_id('<bos>')
+            
+            tgt_out = model.generate(input_ids,bos=bos,deterministic=True)
+
+            print(f"Output token IDs: {tgt_out.tolist()}")
+            tgt_out = bert_tokenizer.decode(tgt_out.numpy())
+    
+            print(f"Decoded output: {tgt_out}")
+            
+            st.write(f"{tgt_out}")
+            
+        except Exception as e:
+            print(f"Error occurred: {str(e)}")
+            print(f"Error type: {type(e)}")
+            import traceback
+            print(traceback.format_exc())
+            st.write("An error occurred while processing your question.")
