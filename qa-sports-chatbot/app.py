c374d8a3a11816bc7514c3dcee7fae9e9583fd63
claratys <158698222+claratys@users.noreply.github.com>
2024-11-10 00:29:25 +0800

    fix issue with models running on other models inputs

--- qa-sports-chatbot/app.py
+++ qa-sports-chatbot/app.py
@@ -9,40 +9,38 @@ st.caption(
     """Chatbot using :rainbow[cross attention and self-attention]"""
 )
 
-# Who is the best basketball player?
-if user_query := st.text_input(
+# Model input
+if user_query1 := st.text_input(
     key="one",
     label="Ask a question about Rugby 🏉, Soccer ⚽ or Basketball 🏀!",
-    placeholder="How many field goals did Kobe Bryant score?",
+    placeholder="How many field goals did Kobe Bryant score?"
 ):
-    # Get the answers
     with st.spinner("Waiting"):
-        answer = utils.generate_ans(user_query)
+        answer = utils.generate_ans(user_query1)
         st.divider()
 
 st.caption(
     """Chatbot using :rainbow[multiplicative attention]"""
 )
-# Who is the best basketball player?
-if user_query := st.text_input(
+
+if user_query2 := st.text_input(
+    key="two",
     label="Ask a question about Rugby 🏉, Soccer ⚽ or Basketball 🏀!",
-    placeholder="How many field goals did Kobe Bryant score?",
+    placeholder="How many field goals did Kobe Bryant score?"
 ):
-    # Get the answers
     with st.spinner("Waiting"):
-        answer = utils_model2.generate_ans(user_query)
+        answer = utils_model2.generate_ans(user_query2)
         st.divider()
 
 st.caption(
     """Chatbot using :rainbow[multiplicative attention] with 512 dim"""
 )
-# Who is the best basketball player?
-if user_query := st.text_input(
+
+if user_query3 := st.text_input(
     key="three",
     label="Ask a question about Rugby 🏉, Soccer ⚽ or Basketball 🏀!",
-    placeholder="How many field goals did Kobe Bryant score?",
+    placeholder="How many field goals did Kobe Bryant score?"
 ):
-    # Get the answers
     with st.spinner("Waiting"):
-        answer = utils_model3.generate_ans(user_query)
+        answer = utils_model3.generate_ans(user_query3)
         st.divider()
